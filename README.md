8ca9377f7466b4ab3b4f482072fd54c7bbe91330
Rahul Yedida <rahul.yedida@pm.me>
2022-06-14 21:57:43 -0700

    Implemented repo initialization

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # raise-tools
 
-This repository contains the source for `raise-tools`, which check repositories by the RAISE lab for compliance with a template.
+This repository contains the source for `raise`, which check repositories by the RAISE lab for compliance with a template.
+
+## Usage
+
+There are two primary use cases for this tool. The first is to check that a directory containing your code is compliant with a RAISE-provided template. To do so, use
+
+```
+raise .
+```
+
+The `.` can be replaced by the directory where your code lives. This will check that your code follows PEP8 standards, that it contains the required files, and that the README file contains the necessary sections.
+
+A second use case is to initialize a new repo from a template, similar to `npm init`. The templates come with some working code already to give you a feel for how code is expected to be written. To initialize, use
+
+```
+raise init --template dl4se
+```
+
+`dl4se` can be replaced by any template name (currently, it is the only one). If the template argument is not provided, `dl4se` is assumed. You can provide configuration options in a file. To do so, name the file `.raise.conf` in the current directory. The configuration file has the syntax below.
+
+**Note:** The options in the configuration file have precedence over command-line args. This means that if you specify the same thing in both places, your command-line args will be ignored. This is to encourage the use of configuration files.
+
+```
+[init]
+template = dl4se  # or any other template
+token = GITHUB_ACCESS_TOKEN
+fork = yes  # or "no"
+
+[check]
+max_line_length = 120
+```
 
 ## Release
 
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+Main code file for the raise tool. Implements compliance checking and repo initialization.
+
+FWIW, I too think it's ridiculous that we need two libraries for GitHub stuff--and we only
+use one function from each; but one does not support the other, so here we fucking are.
+
+Authors:
+    Rahul Yedida <rahul@ryedida.me>
+"""
 import os
 import argparse
 import glob
@@ -5,12 +14,20 @@ from pathlib import Path
 from colorama import Fore, init
 from pycodestyle import StyleGuide
 from typing import Union
+from github import Github
+from git import Repo
+import configparser
+
+
+CONFIG_FILENAME = '.raise.conf'
 
 
 def test_file_exists(base_path: Path, filename: str):
     """
     Checks that a file exists. The return scheme is reversed: this allows us
     to count the ERRORS.
+
+    TODO: Move this to a utils file
 
     :param {Path} base_path - The base path.
     :param {str} filename - The filename to check for.
@@ -26,6 +43,8 @@ def test_string_exists(base_path: Path, filename: str, string: Union[str, list],
     """
     Tests whether a file contains a specific string, optionally at the beginning of a line. As with
     test_file_exists, returns True if the string does NOT exist.
+
+    TODO: Move this to a utils file.
 
     :param {Path} base_path - The base path
     :param {str} filename - The file to test
@@ -56,16 +75,82 @@ def test_string_exists(base_path: Path, filename: str, string: Union[str, list],
     return False
 
 
+def overwrite_args_with_config(args: dict, config: dict):
+    """
+    Overwrites args with preferences in the config file.
+
+    :param {dict} args - the result of vars(ArgumentParser.parse)
+    :config {dict} config - a dict with preferences
+    """
+    if 'init' in config:
+        init_prefs = config['init']
+
+        if 'template' in init_prefs:
+            args['template'] = init_prefs['template']
+
+        if 'fork' in init_prefs:
+            try:
+                args['no_fork'] = not init_prefs.getboolean('fork')
+            except ValueError:
+                print(
+                    Fore.RED + '[ERR] invalid value for init.fork in config file')
+
+    if 'check' in config:
+        check_prefs = config['check']
+
+        if 'max_line_length' in check_prefs:
+            try:
+                args['max_line_length'] = check_prefs.getint('max_line_length')
+            except ValueError:
+                print(
+                    Fore.RED + '[ERR] invalid value for check.max_line_length in config file')
+
+
 def _main():
     # Parse arguments
     parser = argparse.ArgumentParser(
         description='Checks compliance with the RAISE code template.')
     parser.add_argument('path', type=str, help='Root directory')
+    parser.add_argument('init', action='store_true',
+                        help='Initializes a new repo from a template')
+    parser.add_argument('--template', type=str, default='dl4se',
+                        help='Selects the template to use')
     parser.add_argument('--max_line_length', default=120)
-    args = parser.parse_args()
+    args = vars(parser.parse_args())
+
+    # Does a config file exist?
+    if os.path.exists(CONFIG_FILENAME):
+        parser = configparser.ConfigParser()
+        parser.read(CONFIG_FILENAME)
+
+        # Overwrite args with config file preferences
+        overwrite_args_with_config(args, config)
+
+    if args['init']:
+        # Initialize a new repo: fork, and then clone
+        template = args['template']
+
+        if not args.no_fork:
+            # Find the token
+            if 'token' not in args:
+                print('raise will now fork the template repo to your account. ' +
+                      'Please provide a GitHub access token.')
+                token = input(Fore.BLUE + 'access_token = ' + Fore.RESET)
+            else:
+                token = args['token']
+
+            # Fork the repo
+            gh = Github(token)
+            repo = gh.get_repo(f'yrahul3910/raise-template-{template}')
+            repo.create_fork()
+
+            # Clone the repo
+            Repo.clone_from(repo.html_url, os.getcwd())
+
+        return
 
     # Extract root directory
-    base_path = Path(args.path)
+    base_path = Path(args['path'])
 
     # Initialize colorama
     init()
@@ -73,7 +158,7 @@ def _main():
     # Perform pycodestyle checks
     print(Fore.BLUE + '[Running code style checks]')
     print(Fore.RED)
-    checker = StyleGuide(max_line_length=args.max_line_length)
+    checker = StyleGuide(max_line_length=args['max_line_length'])
     report = checker.check_files(paths=glob.glob(
         f'{base_path}/**/*.py', recursive=True))
     report.print_statistics()
